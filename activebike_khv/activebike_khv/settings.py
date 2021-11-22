7695230250d445bf3a3c71e4ecf7efd30f5f5caf
Роман Чуклинов <xjt@mail.ru>
2021-11-23 00:46:06 +1000

    настройка статики

--- activebike_khv/activebike_khv/settings.py
+++ activebike_khv/activebike_khv/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'activebike_khv.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +70,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 WSGI_APPLICATION = 'activebike_khv.wsgi.application'
 
--- activebike_khv/activities/views.py
+++ activebike_khv/activities/views.py
@@ -4,17 +4,16 @@ from django.core.cache import cache
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import CommentForm, PostForm
-from .models import Comment, Follow, Group, Post, User
+# from .forms import CommentForm, PostForm
+# from .models import Comment, Follow, Group, Post, User
 
 def index(request):
-    template = 'posts/index.html'
-    posts = Post.objects.select_related('group')
-    paginator = Paginator(posts, settings.POSTS_PER_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    template = 'activities/index.html'
+    # posts = Post.objects.select_related('group')
+    # paginator = Paginator(posts, settings.POSTS_PER_PAGE)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
     context = {
-        'page_obj': page_obj,
-        'index': True
+        'page_obj': 'hello'
     }
     return render(request, template, context)
