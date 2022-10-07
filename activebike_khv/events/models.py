fa7171e1f2daf728ac3f682061d5d1aa92cbd131
Роман Чуклинов <xjt@mail.ru>
2022-10-07 13:35:34 +1000

    Проработка моделей2

--- activebike_khv/events/models.py
+++ activebike_khv/events/models.py
@@ -5,7 +5,7 @@ from django.db.models.deletion import SET_NULL
 
 User = get_user_model()
 
-class ActivityType(models.Model):
+class EventType(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50, unique=True)
     description = models.TextField()
@@ -14,37 +14,36 @@ class ActivityType(models.Model):
         return self.title
 
     class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
+        verbose_name = 'Тип события'
+        verbose_name_plural = 'Типы событий'
 
-class Activity(models.Model):
+class Event(models.Model):
     caption = models.CharField(max_length=200)
-    text = models.TextField()
+    description = models.TextField()
     image = models.ImageField(
         'Картинка',
-        upload_to='activities/',
+        upload_to='events/',
         blank=True
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='events'
     )
-    date_plane = models.DateTimeField(auto_now_add=True)
-    date_fact = models.DateTimeField(auto_now_add=True)
-    date_edit = models.DateTimeField(auto_now_add=True)
-    a_type = models.ForeignKey(
-        ActivityType,
+    event_type = models.ForeignKey(
+        EventType,
         on_delete=SET_NULL,
         blank=True,
         null=True,
         related_name='activities'
     )
+    date_planned = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.text[:15]
+        return self.caption[:15]
 
     class Meta:
-        ordering = ['-date_edit']
-        verbose_name = 'Мероприятие'
-        verbose_name_plural = 'Мероприятия'
+        ordering = ['-date_edited']
+        verbose_name = 'Событие'
+        verbose_name_plural = 'События'
