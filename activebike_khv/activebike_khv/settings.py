1764d1ae00db981b53973a5dcdd8955d543a83f1
Роман Чуклинов <xjt@mail.ru>
2022-10-12 09:20:22 +1000

    Страница событий

--- activebike_khv/activebike_khv/settings.py
+++ activebike_khv/activebike_khv/settings.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'hhhffdd'
 
@@ -67,7 +68,8 @@ WSGI_APPLICATION = 'activebike_khv.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
