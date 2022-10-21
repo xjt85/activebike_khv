a5224b99a99243f29e77072c7981e50d729d797a
Роман Чуклинов <xjt@mail.ru>
2022-10-21 13:36:27 +1000

    Верстка, страницы статей

--- activebike_khv/events/urls.py
+++ activebike_khv/events/urls.py
@@ -6,5 +6,8 @@ app_name = 'events'
 
 urlpatterns = [
     path('', views.events_index, name='events_index'),
+    path('events/', views.events_index, name='events_index'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('articles/', views.events_index, name='articles_index'),
+    path('articles/<int:article_id>/', views.article_detail, name='article_detail'),
 ]
--- activebike_khv/events/views.py
+++ activebike_khv/events/views.py
@@ -6,7 +6,8 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 # from .forms import CommentForm, PostForm
-from .models import EventType, Event, User
+from .models import EventType, Event, User, Article
+
 
 def events_index(request):
     template = 'events/index.html'
@@ -20,6 +21,7 @@ def events_index(request):
     }
     return render(request, template, context)
 
+
 def event_detail(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     # comments = Comment.objects.filter(event=event)
@@ -32,3 +34,28 @@ def event_detail(request, event_id):
     }
 
     return render(request, 'events/event_detail.html', context)
+
+
+def articles_index(request):
+    template = 'articles/index.html'
+    events = Article.objects.all()
+    # posts = Post.objects.select_related('group')
+    # paginator = Paginator(posts, settings.POSTS_PER_PAGE)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+    context = {
+        'events': events
+    }
+    return render(request, template, context)
+
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    # comments = Comment.objects.filter(event=event)
+    # author = event.author
+    # author_events_count = Event.objects.filter(author=event.author).count()
+    # form = CommentForm(request.POST or None)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/article_detail.html', context)
