fa18445a43444aa0dc6ca4cf62d0ce5d52c9d5e0
Роман Чуклинов <xjt@mail.ru>
2022-10-29 09:05:53 +1000

    Воркфлоу, анализ трека

--- track_analyse.py
+++ track_analyse.py
@@ -1,8 +1,9 @@
 import gpxpy
 import gpxpy.gpx
 import math
+import csv
 
-with open("izubr.gpx", "r", encoding="utf-8") as file:
+with open("300км.gpx", "r", encoding="utf-8") as file:
     gpx = gpxpy.parse(file)
 data = []
 for track in gpx.tracks:
@@ -11,22 +12,26 @@ for track in gpx.tracks:
             data.append([float(point.latitude), float(point.longitude), float(point.elevation)])
 
 
-# print(f'{data[0][0]-data[1][0]};{data[0][1]-data[1][1]};{data[0][2]-data[1][2]}')
-
-
 def get_track_distance(data):
     result = 0
     for i in range(len(data) - 1):
-        x_diff = abs(data[i][0] - data[i+1][0]) * 111200
-        y_diff = abs(data[i][1] - data[i+1][1]) * 111200
-        z_diff = abs(data[i][2] - data[i+1][2])
-        if x_diff < 6 and y_diff < 6 and z_diff < 6:
-            x_diff = 0
-            y_diff = 0
-            z_diff = 0
-        else:
-            result += math.sqrt(x_diff * x_diff + y_diff * y_diff + z_diff * z_diff)
+        x_diff = round(abs(data[i][0] - data[i + 1][0]) * 111200, 2)
+        y_diff = round(abs(data[i][1] - data[i + 1][1]) * 111200, 2)
+        z_diff = round(abs(data[i][2] - data[i + 1][2]), 2)
+        # print(f'{x_diff}, {y_diff}, {z_diff}')
+        result += math.sqrt(pow(x_diff, 2) + pow(y_diff, 2) + pow(z_diff, 2))
     return round(result, 2)
+
+
+# with open('result.csv', 'w', newline='') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=' ',
+#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    
+#     for row in data:
+#         writer.writerow(row)
+
+
+
 
 
 print(get_track_distance(data))
